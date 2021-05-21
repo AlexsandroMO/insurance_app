a10e3f34b2c359c794298a3138320711e598f727
AlexsandroMO <sandrobass@htomail.com>
2021-05-21 10:17:50 -0300

    start-project-mirror

--- insurance_control/settings.py
+++ insurance_control/settings.py
@@ -5,7 +5,6 @@ Django settings for insurance_control project.
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,5 +127,3 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
